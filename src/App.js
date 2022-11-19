b3b5f4fc523d7f10ca71a8e148567faf9ddb4d8b
Alan Xu <alan.xu1699@gmail.com>
2022-11-18 20:39:41 -0500

    setup about, home, and collection routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+import AboutPage from "./components/AboutPage";
+import CollectionPage from "./components/CollectionPage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
-        <h1 className="text-3xl text-blue-500 underline">Hello world</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/CollectionPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CollectionPage = () => {
+    return (
+        <div>
+            <h1>This is CollectionPage</h1>
+        </div>
+    )
+}
+
+export default CollectionPage
