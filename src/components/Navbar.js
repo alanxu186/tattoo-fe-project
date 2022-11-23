df13eb1a22d9465299006c3600665b966e78f614
Alan Xu <alan.xu1699@gmail.com>
2022-11-23 00:06:51 -0500

    border animation test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <div>
-                <h1>Tattoo Shop</h1>
+            <div className='flex h-screen w-full items-center justify-center bg-slate-400'>
+                <h1 className='relative h-20 w-20 border before:absolute before:-bottom-2 before:-right-2 before:h-4 before:w-4 before:border-b before:border-r before:transition-all before:duration-300 before:ease-in-out after:absolute after:-top-2 after:-left-2 after:h-4 after:w-4 after:border-t after:border-l after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]'>Tattoo Shop</h1>
+                <h1 className='relative h-20 w-20 border before:absolute before:-bottom-2 before:-right-2 before:h-4 before:w-4 before:border-b before:border-r before:transition-all before:duration-300 before:ease-in-out after:absolute after:-top-2 after:-left-2 after:h-4 after:w-4 after:border-t after:border-l after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]'>Tattoo Shop</h1>
+
             </div>
         </div>
     )
